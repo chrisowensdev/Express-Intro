58f1e547da45806ac2fffaeeb418317980312a60
Chris Owens <guitarrockstarco@gmail.com>
2020-09-14 15:40:39 -0400

    Added database functionality

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const express = require('express');
 const app = express();
 
 const server = http.createServer(app);
+const db = require('./db');
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}`);
@@ -41,6 +42,33 @@ const helloController = (req, res) => {
         .end();
 }
 
+const monsterController = (req, res) => {
+    let snippet = `<h1> BOOO!!!!</h1>`;
+
+    if (req.params.name === undefined && db !== undefined) {
+        snippet += `<ul>`;
+        db.map(monster => {
+            snippet += `<li><a href="/monster/${monster.slug}">${monster.name}</a></li>`;
+        });
+        snippet += '</ul>';
+    }
+
+    if (req.params.name !== undefined) {
+        db.map(monster => {
+            if (req.params.name === monster.slug) {
+                snippet = `<h1>BOOO!!! It is ${monster.name}</h1>
+                    <p> I am ${monster.name}</p>
+                    <p> Species: ${monster.species}`;
+            }
+        })
+    }
+    res
+        .status(200)
+        .send(snippet)
+        .end();
+}
+
 app.get('/', rootController); // <- ROOT route
 app.get('/foo', fooController); // <- FOO route
 app.get('/hello/:name?', helloController); // <- HELLO route
+app.get('/monster/:name?', monsterController);
